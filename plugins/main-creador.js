2e50ffa213626d8b9f571a25c2a4cc7bc72e6475
KillzN <scorpionsheep203@gmail.com>
2025-06-18 20:04:10 -0400

    KILL BOT

--- plugins/main-creador.js
+++ plugins/main-creador.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
     m.react('👤')
-    let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:KILLBOT;;\nFN:KILLBOT\nORG:KILLBOT\nTITLE:\nitem1.TEL;waid=56983073328:56983073328\nitem1.X-ABLabel:KILLBOT\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:KILLBOT\nEND:VCARD`
+    let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:BʏKɪʟʟᴢN 🦥;;\nFN:BʏKɪʟʟᴢN 🦥\nORG:BʏKɪʟʟᴢN 🦥\nTITLE:\nitem1.TEL;waid=56983073328:56983073328\nitem1.X-ABLabel:BʏKɪʟʟᴢN 🦥\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:BʏKɪʟʟᴢN 🦥\nEND:VCARD`
     await conn.sendMessage(m.chat, { contacts: { displayName: '@ineffable.mvrco⁩', contacts: [{ vcard }] } }, { quoted: m })
 }
 handler.help = ['staff']
